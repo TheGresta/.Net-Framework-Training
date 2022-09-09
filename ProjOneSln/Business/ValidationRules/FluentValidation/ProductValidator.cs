bdaa87f152549af49ed6475e58788faea5b6a401
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-09-09 13:42:22 +0300

    Training #6

---
+++ ProjOneSln/Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,29 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(p => p.ProductName)
+                .NotEmpty()
+                .MinimumLength(2)
+                .Must(StartWithA);
+
+            RuleFor(p => p.UnitPrice)
+                .NotEmpty()
+                .GreaterThan(0)
+                .GreaterThanOrEqualTo(10).When(p => p.CategoryId == 1);
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
